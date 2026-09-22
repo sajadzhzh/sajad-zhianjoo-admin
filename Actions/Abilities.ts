30ce90f2295bef8f06b4cbbcd0431602a30e5733
Sajadzhzh <zhiyanjoo1382@gmail.com>
2026-09-22 16:23:10 +0330

    Abilities done

--- Actions/Abilities.ts
+++ Actions/Abilities.ts
@@ -1,7 +1,8 @@
 "use server";
 
-import { getFetch } from "@/Helper/Fetch";
+import { delFetch, getFetch, postFetch, putFetch } from "@/Helper/Fetch";
 import Response from "@/Helper/Response";
+import { cookies } from "next/headers";
 
 export async function GetAllAbilities() {
   try {
@@ -11,6 +12,171 @@ export async function GetAllAbilities() {
       return Response({
         success: res.success,
         data: res.data,
+      });
+    } else {
+      return Response({
+        success: res.success,
+        message: res.message,
+      });
+    }
+  } catch (e: any) {
+    console.log(e.message);
+
+    return Response({
+      success: false,
+      message: "مشکلی پیش آمد. لطفا مجدد تلاش کنید.",
+    });
+  }
+}
+
+export async function GetAbilityById(id: string) {
+  try {
+    const res = await getFetch(`abilities/${id}`);
+
+    if (res.success) {
+      return Response({
+        success: res.success,
+        data: res.data,
+      });
+    } else {
+      return Response({
+        success: res.success,
+        message: res.message,
+      });
+    }
+  } catch (e: any) {
+    console.log(e.message);
+
+    return Response({
+      success: false,
+      message: "مشکلی پیش آمد. لطفا مجدد تلاش کنید.",
+    });
+  }
+}
+
+export async function DeleteAbility(id: string) {
+  const token = (await cookies()).get("token")?.value;
+
+  if (!token) {
+    return Response({
+      success: false,
+      message: "توکن شما پاک یا منقضی شده است. لطفا دوباره وارد شوید.",
+    });
+  }
+  
+  try {
+    const res = await delFetch(`abilities/${id}`, {
+      Authorization: `Bearer ${token}`,
+    });
+
+    if (res.success) {
+      return Response({
+        success: res.success,
+        message: res.message,
+      });
+    } else {
+      return Response({
+        success: res.success,
+        message: res.message,
+      });
+    }
+  } catch (e: any) {
+    console.log(e.message);
+
+    return Response({
+      success: false,
+      message: "مشکلی پیش آمد. لطفا مجدد تلاش کنید.",
+    });
+  }
+}
+
+export async function NewAbility({
+  name,
+  sort,
+}: {
+  name: string;
+  sort: string;
+}) {
+  const token = (await cookies()).get("token")?.value;
+
+  if (!token) {
+    return Response({
+      success: false,
+      message: "توکن شما پاک یا منقضی شده است. لطفا دوباره وارد شوید.",
+    });
+  }
+
+  if (!name || !sort) {
+    return Response({
+      success: false,
+      message: "تمام بخش ها الزامی هستند!",
+    });
+  }
+
+  try {
+    const res = await postFetch(
+      "abilities",
+      { name, sort },
+      { Authorization: `Bearer ${token}` },
+    );
+
+    if (res.success) {
+      return Response({
+        success: res.success,
+        message: res.message,
+      });
+    } else {
+      return Response({
+        success: res.success,
+        message: res.message,
+      });
+    }
+  } catch (e: any) {
+    console.log(e.message);
+
+    return Response({
+      success: false,
+      message: "مشکلی پیش آمد. لطفا مجدد تلاش کنید.",
+    });
+  }
+}
+
+export async function EditAbility({
+  name,
+  sort,
+  id,
+}: {
+  name: string;
+  sort: string;
+  id: string;
+}) {
+  const token = (await cookies()).get("token")?.value;
+
+  if (!token) {
+    return Response({
+      success: false,
+      message: "توکن شما پاک یا منقضی شده است. لطفا دوباره وارد شوید.",
+    });
+  }
+
+  if (!name || !sort) {
+    return Response({
+      success: false,
+      message: "تمام بخش ها الزامی هستند!",
+    });
+  }
+
+  try {
+    const res = await putFetch(
+      `abilities/${id}`,
+      { name, sort },
+      { Authorization: `Bearer ${token}` },
+    );
+
+    if (res.success) {
+      return Response({
+        success: res.success,
+        message: res.message,
       });
     } else {
       return Response({
--- Components/Abilities/Form.tsx
+++ Components/Abilities/Form.tsx
@@ -4,6 +4,14 @@ import { useEffect, useState } from "react";
 import SelectBox from "../Input/SelectBox";
 import TextInput from "../Input/Text";
 import Button from "../Button/Button";
+import toast from "react-hot-toast";
+import {
+  DeleteAbility,
+  EditAbility,
+  GetAbilityById,
+  NewAbility,
+} from "@/Actions/Abilities";
+import { useRouter } from "next/navigation";
 
 export default function AbilityForm({
   edit,
@@ -13,17 +21,84 @@ export default function AbilityForm({
   id?: string;
 }) {
   const [sort, setSort] = useState("");
+  const [name, setName] = useState("");
+  const router = useRouter();
+
   useEffect(() => {
-    edit && setSort("front");
+    const request = async () => {
+      if (id) {
+        const res = await GetAbilityById(id);
+
+        if (res.success) {
+          setName(res.data.name);
+          setSort(res.data.sort);
+        } else {
+          setName("");
+          setSort("");
+          res.message && toast.error(res.message);
+        }
+      }
+    };
+    if (edit) {
+      request();
+    }
   }, []);
 
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if (!name || !sort) {
+      toast.error("تمام بخش ها الزامی هستند!");
+    }
+
+    try {
+      const res =
+        edit && id
+          ? await EditAbility({ name, sort, id })
+          : await NewAbility({ name, sort });
+
+      if (res.success) {
+        res.message && toast.success(res.message);
+        setTimeout(() => {
+          router.push("/abilities");
+        }, 2000);
+      } else {
+        res.message && toast.error(res.message);
+      }
+    } catch (e: any) {
+      toast.error("مشکلی در ارسال و دریافت اطلاعات پیش آمد.");
+    }
+  };
+
+  const handleDelete = async () => {
+    if (id) {
+      const res = await DeleteAbility(id);
+
+      if (res.success) {
+        res.message && toast.success(res.message);
+        setTimeout(() => {
+          router.push("/abilities");
+        }, 2000);
+      } else {
+        res.message && toast.error(res.message);
+      }
+    }
+  };
   return (
-    <form className="space-y-2 w-full grid grid-cols-1 md:grid-cols-2 gap-2">
+    <form
+      className="space-y-2 w-full grid grid-cols-1 md:grid-cols-2 gap-2"
+      onSubmit={(e) => handleSubmit(e)}
+    >
       <div className="w-full flex flex-col gap-2">
         <label htmlFor="name" className="text-[14px]">
           عنوان مهارت
         </label>
-        <TextInput name="name" id="name" defaultValue={edit ? "Next.js" : ""} />
+        <TextInput
+          name="name"
+          id="name"
+          defaultValue={edit ? name : ""}
+          onChange={(e) => setName(e.target.value)}
+        />
       </div>
 
       <div className="w-full flex flex-col gap-2">
@@ -32,21 +107,22 @@ export default function AbilityForm({
           value={sort}
           setValue={setSort}
           options={[
-            { label: "front", value: "front" },
-            { label: "back", value: "back" },
-            { label: "total", value: "total" },
+            { label: "فرانت‌اند", value: "فرانت‌اند" },
+            { label: "بک‌اند", value: "بک‌اند" },
+            { label: "مهارت عمومی", value: "مهارت عمومی" },
           ]}
         />
       </div>
 
       <div className="md:col-span-2 grid grid-cols-1 md:grid-cols-2 gap-2">
-        <Button theme="primary">
+        <Button theme="primary" type="submit">
           {edit ? "اعمال تغییرات" : "ایجاد مهارت"}
         </Button>
 
         {edit && (
           <Button
             theme="normal"
+            onClick={handleDelete}
             className="border border-red-600 bg-red-500/50 hover:bg-red-500/75"
           >
             حذف مهارت

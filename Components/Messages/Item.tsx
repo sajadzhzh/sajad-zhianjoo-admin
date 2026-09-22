4d466f2474368bb19bf1ec9a54dd9e8c4c90037a
Sajadzhzh <zhiyanjoo1382@gmail.com>
2026-09-22 17:24:20 +0330

    messages page done

--- Components/Messages/Item.tsx
+++ Components/Messages/Item.tsx
@@ -4,9 +4,20 @@ import Link from "next/link";
 import "./Messages.css";
 import { useState } from "react";
 import Modal from "../Modal/Modal";
+import { toJalaliDate } from "@/Helper/Date";
 
-export default function MessageItem() {
+type MessageType = {
+  id: number;
+  name: string;
+  title: string;
+  text: string;
+  email: string;
+  created_at: string;
+};
+
+export default function MessageItem({ data }: { data: MessageType }) {
   const [show, setShow] = useState(false);
+
   return (
     <>
       <Link
@@ -14,32 +25,20 @@ export default function MessageItem() {
         className="message-item grid grid-cols-2 md:grid-cols-4"
         onClick={() => setShow(true)}
       >
-        <p>نام مشترک</p>
-        <p>موضوع پیام</p>
-        <p>ایمیل</p>
-        <p>زمان ارسال</p>
+        <p>{data.name}</p>
+        <p>{data.title}</p>
+        <p>{data.email}</p>
+        <p>{toJalaliDate(data.created_at)}</p>
       </Link>
 
       <Modal show={show} setShow={setShow}>
         <div className="w-full grid grid-cols-1 sm:grid-cols-2 gap-2 justify-items-center sm:justify-items-normal border-b border-(--border) pb-2 text-[14px] text-(--muted)">
-          <p>نام مشترک</p>
-          <p>موضوع پیام</p>
-          <p>ایمیل</p>
-          <p>زمان ارسال</p>
+          <p>{data.name}</p>
+          <p>{data.title}</p>
+          <p>{data.email}</p>
+          <p>{toJalaliDate(data.created_at)}</p>
         </div>
-        <p className="text-[14px] text-justify py-4">
-          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با
-          استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در
-          ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و
-          کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی
-          در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می
-          طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
-          الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این
-          صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و
-          شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای
-          اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده
-          قرار گیرد.
-        </p>
+        <p className="text-[14px] text-justify py-4">{data.text}</p>
       </Modal>
     </>
   );
--- app/(main)/messages/page.tsx
+++ app/(main)/messages/page.tsx
@@ -1,25 +1,34 @@
+"use client";
+
+import { GetAllMessages } from "@/Actions/Messages";
 import MessageItem from "@/Components/Messages/Item";
+import { useEffect, useState } from "react";
 
 export default function MessagesPage() {
+  const [messages, setMessages] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const request = async () => {
+      const res = await GetAllMessages();
+
+      if (res.success) {
+        setMessages(res.data);
+        setLoading(false);
+      } else {
+        setMessages([]);
+        setLoading(false);
+      }
+    };
+    request();
+  }, []);
+
   return (
     <div className="w-full h-[90svh] overflow-scroll Container space-y-5">
       <h2 className="font-bold text-[16px]">پیام‌ها</h2>
 
       <div className="w-full grid grid-cols-1 gap-2">
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
-        <MessageItem />
+        {messages.length > 0 && messages.map((i, index) => <MessageItem key={index} data={i} />)}
       </div>
     </div>
   );

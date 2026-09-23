91cfb4b00703ff326c481da6ccfeccc315e4eb15
Sajadzhzh <zhiyanjoo1382@gmail.com>
2026-09-23 23:13:50 +0330

    site-settings page socialMedia done

---
+++ Actions/SiteSettings.ts
@@ -0,0 +1,85 @@
+"use server";
+
+import { getFetch, putFetch } from "@/Helper/Fetch";
+import Response from "@/Helper/Response";
+import { cookies } from "next/headers";
+
+export async function GetSocialMedia() {
+  try {
+    const res = await getFetch("info/social-media");
+
+    if (JSON.parse(res.success)) {
+      return Response({
+        success: res.success,
+        data: res.data,
+      });
+    } else {
+      return Response({
+        success: res.success,
+        message: res.message,
+      });
+    }
+  } catch (e: any) {
+    console.log(e.message);
+
+    return Response({
+      success: false,
+      message: "مشکلی پیش آمد. لطفا مجدد تلاش کنید.",
+    });
+  }
+}
+
+export async function UpdateSocialMedia({
+  instagram,
+  telegram,
+  github,
+  linkedin,
+}: {
+  instagram: string;
+  telegram: string;
+  github: string;
+  linkedin: string;
+}) {
+  const token = (await cookies()).get("token")?.value;
+
+  if (!token) {
+    return Response({
+      success: false,
+      message: "توکن شما پاک یا منقضی شده است. لطفا مجدد وارد شوید.",
+    });
+  }
+
+  if (!instagram || !telegram || !github || !linkedin) {
+    return Response({
+      success: false,
+      message: "تمام بخش‌ها الزامی هستند!",
+    });
+  }
+
+  try {
+    const res = await putFetch(
+      "info/social-media",
+      { instagram, telegram, github, linkedin },
+      { Authorization: `Bearer ${token}` },
+    );
+
+    if (JSON.parse(res.success)) {
+      return Response({
+        success: res.success,
+        message: res.message,
+      });
+    } else {
+      return Response({
+        success: res.success,
+        message: res.message,
+      });
+    }
+  } catch (e: any) {
+    console.log(e.message);
+
+    return Response({
+      success: false,
+      message: "مشکلی پیش آمد. لطفا مجدد تلاش کنید.",
+    });
+  }
+}
--- Components/site-settings/LinksForm.tsx
+++ Components/site-settings/LinksForm.tsx
@@ -1,35 +1,115 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Button from "../Button/Button";
 import TextInput from "../Input/Text";
+import { GetSocialMedia, UpdateSocialMedia } from "@/Actions/SiteSettings";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 export default function LinksForm() {
+  const [instagram, setInstagram] = useState("");
+  const [telegram, setTelegram] = useState("");
+  const [github, setGithub] = useState("");
+  const [linkedin, setLinkedin] = useState("");
+  const router = useRouter();
+
+  useEffect(() => {
+    const request = async () => {
+      const res = await GetSocialMedia();
+
+      if (res.success) {
+        setInstagram(res.data.instagram);
+        setTelegram(res.data.telegram);
+        setGithub(res.data.github);
+        setLinkedin(res.data.linkedin);
+      } else {
+        setInstagram("");
+        setTelegram("");
+        setGithub("");
+        setLinkedin("");
+      }
+    };
+    request();
+  }, []);
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    if (!instagram || !telegram || !github || !linkedin) {
+      toast.error("تمام لینک ها الزامی هستند!");
+    }
+
+    try {
+      const res = await UpdateSocialMedia({
+        instagram,
+        telegram,
+        github,
+        linkedin,
+      });
+
+      if (res.success) {
+        res.message && toast.success(res.message);
+        router.refresh();
+      } else {
+        res.message && toast.error(res.message);
+      }
+    } catch {
+      toast.error("مشکلی در ارسال و دریافت مشکلات پیش آمد.");
+    }
+  };
+
   return (
-    <form className="w-full grid grid-cols-1 md:grid-cols-2 gap-2">
+    <form
+      className="w-full grid grid-cols-1 md:grid-cols-2 gap-2"
+      onSubmit={(e) => handleSubmit(e)}
+    >
       <div className="flex flex-col gap-2">
         <label htmlFor="instagram" className="text-[14px]">
           آدرس اینستاگرام
         </label>
-        <TextInput name="instagram" id="instagram" />
+        <TextInput
+          name="instagram"
+          id="instagram"
+          defaultValue={instagram}
+          onChange={(e) => setInstagram(e.target.value)}
+        />
       </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="telegram" className="text-[14px]">
           آدرس تلگرام
         </label>
-        <TextInput name="telegram" id="telegram" />
+        <TextInput
+          name="telegram"
+          id="telegram"
+          defaultValue={telegram}
+          onChange={(e) => setTelegram(e.target.value)}
+        />
       </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="github" className="text-[14px]">
           آدرس گیت هاب
         </label>
-        <TextInput name="github" id="github" />
+        <TextInput
+          name="github"
+          id="github"
+          defaultValue={github}
+          onChange={(e) => setGithub(e.target.value)}
+        />
       </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="linkedin" className="text-[14px]">
           آدرس لینکدین
         </label>
-        <TextInput name="linkedin" id="linkedin" />
+        <TextInput
+          name="linkedin"
+          id="linkedin"
+          defaultValue={linkedin}
+          onChange={(e) => setLinkedin(e.target.value)}
+        />
       </div>
 
-      <Button theme="primary">
+      <Button theme="primary" type="submit">
         ثبت
       </Button>
     </form>
